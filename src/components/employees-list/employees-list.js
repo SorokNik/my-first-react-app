c366135e056ff6139533ce459d509036e83abcf7
Sorokin Nikita <sorokin.nik@live.com>
2023-02-08 00:38:24 +0300

    add layout

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,15 @@
+import EmployessListItem from '../employees-list-item/employees-list-item';
+
+import './employees-list.css'
+
+const EmployeesList = () => {
+    return (
+        <ul className="app-list list-group">
+            <EmployessListItem/>
+            <EmployessListItem/>
+            <EmployessListItem/>
+        </ul>
+    );
+};
+
+export default EmployeesList;
